e8ff90a79af3d57c48c9b59b9e6730d6a511fe60
LegendisM <makenkap@gmail.com>
2022-08-23 09:46:20 +0430

    main structure (2/5)

---
+++ lib/home/services/document_service.dart
@@ -0,0 +1,10 @@
+import 'package:quiz/home/services/storage_service.dart';
+import 'package:quiz/home/services/location_service.dart';
+import 'package:quiz/home/utils/date_converter.dart';
+import 'package:quiz/home/utils/multiplication.dart';
+
+class DocumentService {
+  //! Create New Document with Structure => check Has Permission To Get Directory Path => if not => alert -> goto settings for get this
+
+  //! Create New Document with Structure And Write This Data To Form => check Has Permission To Get Directory Path => if not => alert -> goto settings for get this
+}
---
+++ lib/home/services/location_service.dart
@@ -0,0 +1,9 @@
+import 'package:geolocator/geolocator.dart';
+
+class LocationService {
+  //! check activated GPS Option On Phone
+
+  //! check application has permission to use this option
+
+  //! get Current Location
+}
