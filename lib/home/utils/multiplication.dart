2d8b2d3e52042b2a4eef008dd2666c98ec688818
LegendisM <makenkap@gmail.com>
2022-08-23 21:34:16 +0430

    Update multiplication.dart

--- lib/home/utils/multiplication.dart
+++ lib/home/utils/multiplication.dart
@@ -1,3 +1,11 @@
-Map calculateMultiplication(int taretNumber) {
-  return Map();
+Map calculateMultiplication(int maxNumber) {
+  Map result = {};
+
+  for (int i = 1; i <= maxNumber; i++) {
+    result[i] = {};
+    for (int i2 = 1; i2 <= 10; i2++) {
+      result[i]["$i * $i2"] = i * i2;
+    }
+  }
+  return result;
 }
